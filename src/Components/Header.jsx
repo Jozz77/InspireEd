2eb6760d4c3496d2ea1d6d602e1cd832eec31c51
Jozz77 <mmadujonathan@gmail.com>
2023-10-10 05:41:42 +0100

    create the next js app

---
+++ src/Components/Header.jsx
@@ -0,0 +1,30 @@
+'use client'
+
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import Logo from './Assets/Logo.svg'
+
+export default function Header() {
+    const router = useRouter()
+  return (
+    <div className='px-[5%] py-6 fixed top-0 w-full flex justify-between text-[white] '>
+        <section className=' w-[15%] '>
+            <Image 
+                src={Logo}
+                alt="Vercel Logo"
+                className='w-full cursor-pointer'
+                onClick={() => router.push('/')}
+            />
+        </section>
+        <nav className='w-[40%] flex justify-between'>
+        <Link href="/about">About</Link>
+        <Link href="/academics">Academics</Link>
+        <Link href="/faq">FAQ</Link>
+        <Link href="/reviews">Reviews</Link>
+        <Link href="/contact">Contact</Link>
+        </nav>
+    </div>
+  )
+}
---
+++ src/app/contact/page.jsx
@@ -0,0 +1,15 @@
+import Header from '@/Components/Header'
+import React from 'react'
+
+export const metadata = {
+  title: 'Contact',
+}
+
+export default function page() {
+  return (
+    <div className=' bg-black h-64 '>
+      <Header />
+      Contact
+    </div>
+  )
+}
