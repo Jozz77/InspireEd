1ad6d708cd42fb4ccfa88fd869f9c4286c5e62e9
Jozz77 <mmadujonathan@gmail.com>
2023-10-23 14:48:14 +0100

    added back

--- src/app/faq/page.jsx
+++ src/app/faq/page.jsx
@@ -7,13 +7,13 @@ import Curriculum from './Components/Curriculum'
 import Activity from './Components/Activity'
 import Get from '../contact/Components/Get'
 import Address from '@/Components/HomePage/Address'
-// import FaqHero from './Components/FaqHero'
+import FaqHero from './Components/FaqHero'
 
 export default function page() {
   return (
     <div>
       <Header />
-      {/* <FaqHero /> */}
+      <FaqHero />
       <General />
       <Curriculum />
       <Activity />
