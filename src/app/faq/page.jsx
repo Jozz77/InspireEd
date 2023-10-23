fe9a55c9a22e2788f1a7756f5cde8ddb3380cb3f
Jozz77 <mmadujonathan@gmail.com>
2023-10-23 11:47:47 +0100

    add new

--- src/app/faq/page.jsx
+++ src/app/faq/page.jsx
@@ -1,12 +1,13 @@
 import Footer from '@/Components/Footer'
 import Header from '@/Components/Header'
 import React from 'react'
-import FaqHero from './Components/FaqHero'
+// import FaqHero from './Components/FaqHero'
 import General from './Components/General'
 import Curriculum from './Components/Curriculum'
 import Activity from './Components/Activity'
 import Get from '../contact/Components/Get'
 import Address from '@/Components/HomePage/Address'
+import FaqHero from './Components/FaqHero'
 
 export default function page() {
   return (
